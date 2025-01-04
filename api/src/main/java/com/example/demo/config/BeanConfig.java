e22258f65e6b3ef353000dba2ed0c8963333a706
Dimitar Nikolov <dim_nikol@abv.bg>
2025-01-04 03:34:40 +0200

    feat: created S3 MiniO bean

--- api/src/main/java/com/example/demo/config/BeanConfig.java
+++ api/src/main/java/com/example/demo/config/BeanConfig.java
@@ -1,12 +1,20 @@
 package com.example.demo.config;
 
 import com.example.demo.model.CategoryEntity;
+import io.minio.MinioClient;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class BeanConfig {
+    @Value("${minio.url}")
+    private String minioUrl;
+    @Value("${minio.access-key}")
+    private String minioAccess;
+    @Value("${minio.secret-key}")
+    private String minioSecret;
 
     @Bean
     public ModelMapper modelMapper() {
@@ -18,6 +26,12 @@ public class BeanConfig {
         return modelMapper;
     }
 
-    // TODO add MinioClient bean
+    @Bean
+    public MinioClient minioClient() {
+        return MinioClient.builder()
+                .endpoint(minioUrl)
+                .credentials(minioAccess, minioSecret)
+                .build();
+    }
 
 }
